e1e0abbbc4beb14709de8d21691601310f2443b9
Nathan <Nathan@Graves-PC.att.net>
2015-06-17 23:21:33 -0500

    Merge origin/master
    Nathan - Adjustments to GameMenuView and View Superclass so as to allow for proper next View and proper Exit of the GameMenuView based upon input

--- SecretsOfTheSea_RPG/src/byui/cit260/secretsOfTheSea/view/View.java
+++ SecretsOfTheSea_RPG/src/byui/cit260/secretsOfTheSea/view/View.java
@@ -20,18 +20,6 @@ public abstract class View implements ViewInterface {
         this.display();
     }
     
-//    @Override
-//        public GameMenuView(){
-//        String menu = this.displayGameMenu();
-//        char selection = ' ';
-//        do {
-//                System.out.println(menu);
-//		selection = this.getInput(menu);
-//		this.nextAction(selection);
-//        }while ( !( selection =='Q' || selection == 'C'  ));
-//    }//Repeat until the player chooses to close the menu or quit the game.
-    
-    
     @Override
     public void display(){
         boolean done = false;
@@ -40,36 +28,29 @@ public abstract class View implements ViewInterface {
                 System.out.println(this.promptMessage);
 		value = this.getInput();
 		done = this.doAction(value);
+                //doAction in sub-class return true to stay in
+                //same view.  return false to end the view and return to previous view
+                //also return false from doAction when an invalid value is input.
         }while (!done);
     }//Repeat until the player chooses to close the menu or quit the game.
     
-        
-//        String tempMenu = null;
-//        tempMenu = "Game Menu Options"
-//                + "\nS - Save Game"
-//                + "\nH - Help"
-//                + "\nQ - Quit Game"
-//                + "\nC - Close Menu"
-//                + "\nI - Inventory Manager"
-//                + "\nV - View all Statuses\n";
-//        return tempMenu;
-//    }
+
     
     @Override
     public char getInput(){
         Scanner keyboard = new Scanner(System.in);
         char value = ' ';
-        boolean valid = false;
+        //boolean valid = false;
             //honestly, this just continues until the if is skipped and the break is executed.
             //if it wasn't for that it woudl continue forever.
-        while(!valid){
+        //while(!valid){
             value = Character.toUpperCase(keyboard.next().charAt(0));
-            if ( !( value == 'S' || value == 'H' || value =='Q' || value == 'C' || 
-                    value=='I' || value=='V' )){
-                System.out.println( "Invalid entry. \n" );
-            }
-            break;
-        }
+        //    if ( !( value == 'S' || value == 'H' || value =='Q' || value == 'C' || 
+        //            value=='I' || value=='V' )){
+        //        System.out.println( "Invalid entry. \n" );
+        //    }
+        //    break;
+        //}
         
         return value;  //return the user input.
     }
@@ -81,6 +62,30 @@ public abstract class View implements ViewInterface {
     public void setPromptMessage(String promptMessage) {
         this.promptMessage = promptMessage;
     }
+    
+    
+//    @Override
+//        public GameMenuView(){
+//        String menu = this.displayGameMenu();
+//        char selection = ' ';
+//        do {
+//                System.out.println(menu);
+//		selection = this.getInput(menu);
+//		this.nextAction(selection);
+//        }while ( !( selection =='Q' || selection == 'C'  ));
+//    }//Repeat until the player chooses to close the menu or quit the game.
+   
+    
+//        String tempMenu = null;
+//        tempMenu = "Game Menu Options"
+//                + "\nS - Save Game"
+//                + "\nH - Help"
+//                + "\nQ - Quit Game"
+//                + "\nC - Close Menu"
+//                + "\nI - Inventory Manager"
+//                + "\nV - View all Statuses\n";
+//        return tempMenu;
+//    }
     
 }
 
